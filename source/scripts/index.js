d995a329460d469187ca53419af64a5a1abcec97
Александра <alexandra.awerjanova@yandex.ru>
2025-08-08 14:03:41 +0300

    декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,19 @@
 /* в этот файл добавляет скрипты*/
+['.header', '.menu-nav', '.contacts__maps'].forEach((selector) =>
+  document
+    .querySelector(selector)
+    ?.classList.remove(`${selector.split('.')[1]}--nojs`)
+);
+
+document.getElementById('burger')?.addEventListener('click', () => {
+  const menuNav = document.querySelector('.menu-nav__list');
+  if (menuNav) {
+    menuNav.classList.toggle('active');
+  }
+
+  const toggleBtn = document.querySelector('.menu-nav__toggle');
+  if (toggleBtn) {
+    toggleBtn.classList.toggle('active');
+  }
+});
+
